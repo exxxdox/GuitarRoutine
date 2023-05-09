b6ac4cf5a305c3904cffe5c9c2b06812d96a7684
tst <417004150@qq.com>
2023-05-09 21:16:21 +0800

    routine 1.0

--- guitar_routine.c
+++ guitar_routine.c
@@ -4,8 +4,11 @@
 #include <string.h>
 
 int KEY = 0;
+char KEYNAME[3];
 int MODE = 0;
 int ARPE = 0;
+
+const char NAME[] = "ABCDEFG";
 
 const int Ionian[] = {0, 2, 4, 5, 7, 9, 11};
 const int Dorian[] = {0, 2, 3, 5, 7, 9, 10};
@@ -15,58 +18,37 @@ const int Mixolydian[] = {0, 2, 4, 5, 7, 9, 10};
 const int Aeolian[] = {0, 2, 3, 5, 7, 8, 10};
 const int Locrian[] = {0, 1, 3, 5, 6, 8, 10};
 
-int CheckDistance(int notea, int howa, int noteb, int howb) {
-//    int level[] = {0, 2, 4, 5, 7, 9, 11};
-    //
-    int oria = notea + howa;
-    int orib = noteb + howb;
-    int distance = abs(oria - orib);
-    return distance;
+int FindOff(char input) {
+    return (int) (input - 'A');
 }
 
-int FindHow(char a, char b) {
-    int result = 0;
-    if (a == ' ') {
-        if (b == '#')
-            result = -1;
-        else if (b == 'b')
-            result = 1;
-    } else if (a == 'b')
-        result = 2;
+int GetANote(char input) {
+    char notes[] = "C D EF G A B";
+    for (int i = 0; i < 12; i++) {
+        if (input == notes[i])
+            return i;
+    }
+}
+
+int FindDistance(int a, int b) {
+    if (abs(a - b) > 2)
+        return a - b - 12;
     else
-        result = -2;
-    return result;
+        return a - b;
 }
 
-char *GetNote(int num, int how) {
-    //how
-    //0 原;1 b;-1 #;
-    num = num + 1;
+char *GetNoteForKey(int num) {
     //记得free()
     char notes[] = "C D EF G A B";
-    if (abs(how) < 2 && (num == 1 || num == 3 || num == 5 || num == 6 || num == 8 || num == 10 || num == 12)) {
-        char a[3] = {' ', ' ', notes[num - 1], '\0'};
+    if (num == 0 || num == 2 || num == 4 || num == 5 || num == 7 || num == 9 || num == 11) {
+        char a[] = {' ', ' ', notes[num], '\0'};
 
         char *b = malloc((strlen(a) + 1) * sizeof(char));
         strcpy(b, a);
         return b;
     } else {
         //用b表达
-        char a[3] = {' ', 'b', notes[num], '\0'};
-        //用#表达
-        if (how == -1) {
-            a[1] = '#';
-            a[2] = notes[(num - 2 + 12) % 12];
-        } else if (how == -2) {
-            a[0] = '#';
-            a[1] = '#';
-            a[2] = notes[(num - 1 + 12) % 12];
-        } else if (how == 2) {
-            a[0] = 'b';
-            a[1] = 'b';
-            a[2] = notes[(num + 1 + 12) % 12];
-        }
-
+        char a[] = {' ', 'b', notes[num + 1], '\0'};
         char *b = malloc((strlen(a) + 1) * sizeof(char));
         strcpy(b, a);
         return b;
@@ -74,22 +56,31 @@ char *GetNote(int num, int how) {
 }
 
 void ShowNote(char *notes) {
-    printf("%s", notes);
+//    printf("%s", notes);
+    for (char *i = notes; *i != '\0'; i++) {
+        if (*i != ' ')
+            printf("%c", *i);
+    }
 }
 
 void ShowKey() {
     int key = rand() % 12;
-    key = 6;
+//    key = 3;
     KEY = key;
-    char *charKey = GetNote(key, 1);
+//    printf("%d", key);
+
+    char *charKey = GetNoteForKey(key);
+
     ShowNote(charKey);
+    strcpy(KEYNAME, charKey);
+
     free(charKey);
 }
 
 void ShowMode() {
     char a[7][15] = {"Ionian", "Dorian", "Phrygian", "Lydian", "Mixolydian", "Aeolian", "Locrian"};
     int num = rand() % 7;
-    num = 1;
+//    num = 1;
     MODE = num;
     char *x = a[num];
     printf("%s", x);
@@ -122,36 +113,42 @@ void ShowScaleDetail() {
             break;
     }
 
-    int last[2];
-    char *name = GetNote(KEY, 1);
-    ShowNote(name);
+
+    int scaleSize = sizeof(s) / sizeof(int);
+//    int scale[scaleSize];
+
+    ShowNote(KEYNAME);
     printf(" ");
-    last[0] = FindHow(name[0], name[1]);
-    last[1] = KEY;
-    free(name);
 
-    for (int i = 1; i < sizeof(s) / sizeof(int); i++) {
+    int oldNum = KEY;
+    int noteOff = FindOff(KEYNAME[2]);
+    for (int i = 1; i < scaleSize; i++) {
         int num = (KEY + s[i]) % 12;
-        char *note;
 
-        note = GetNote(num, 1);
-        int h = FindHow(note[0], note[1]);
-        int distance = CheckDistance(last[1], last[0], num, h);
-        if (distance != 1 && distance != 2) {
-            free(note);
-            if (distance == 0) {
-                note = GetNote(num, 2);
-                h = 2;
-            } else {
-                note = GetNote(num, -1);
-                h = -1;
-            }
+        noteOff = (noteOff + 1) % 7;
+        char note[] = {' ', ' ', NAME[noteOff], '\0'};
+        int adjust = FindDistance(num, GetANote(NAME[noteOff]));
+        switch (adjust) {
+            case -1:
+                note[1] = 'b';
+                break;
+            case -2:
+                note[0] = 'b';
+                note[1] = 'b';
+                break;
+            case 1:
+                note[1] = '#';
+                break;
+            case 2:
+                note[0] = '#';
+                note[1] = '#';
+                break;
+            default:
+                break;
         }
-        last[0] = h;
-        last[1] = num;
 
         ShowNote(note);
-        free(note);
+
         printf(" ");
     }
 }
@@ -183,7 +180,6 @@ int main() {
     ShowScaleDetail();
     printf("\n");
 
-
     printf("和弦练习: ");
     ShowChord();
     printf("\n");
@@ -191,6 +187,6 @@ int main() {
     ShowArpeggio();
     printf("\n");
 
-    //getchar();
+    getchar();
     return 0;
 }
